efa23bc1251cdeb4e9da790e1d48ab7472e05453
M. SYADDAD.G.AL FAREZ <muhammadsyaddad31@gmail.com>
2022-03-09 17:19:15 +0700

    Add files via upload

---
+++ Tutorial Java/13 - Operator Assignments/src/com/tutorial/Main.java
@@ -0,0 +1,32 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        // Operator Assignment
+
+        // Assignment Penjumlahan
+        int penjumlahan = 10;
+        penjumlahan += 5; // penjumlahan = penjumlahan + 5
+        System.out.println(penjumlahan);
+
+        // Assignment Pengurangan
+        int Pengurangan = 10;
+        Pengurangan -= 5; // Pengurangan = Pengurangan - 5
+        System.out.println(Pengurangan);
+
+        // Assignment perkalian
+        int perkalian = 10;
+        perkalian *= 5; // perkalian = perkalian * 5
+        System.out.println(perkalian);
+
+        // Assignment pembagian
+        int pembagian = 10;
+        pembagian /= 5; // pembagian = pembagian / 5
+        System.out.println(pembagian);
+
+        // Assignment modulus
+        int modulus = 10;
+        modulus %= 5; // modulus = modulus % 5
+        System.out.println(modulus);
+    }
+}
